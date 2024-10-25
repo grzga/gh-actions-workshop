9697e1bea8ee58f80c13e4df185cb88cdf641b6a
Grzegorz Gajda <grzegorz.gajda@tngtech.com>
2024-10-25 15:27:37 +0200

    display more playwright info

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
+  console.log(process.env);
+  console.log(test.info());
   await page.goto('/gh-actions-workshop/');
 
   await expect(page).toHaveTitle(/GitHub Actions Workshop/);
